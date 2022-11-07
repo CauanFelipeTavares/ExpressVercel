7ca28f3f36f4580ff4d9fbdacdc34b0a2b656d86
Kali <tavarescauanf@gmail.com>
2022-11-07 02:47:44 -0300

    Sistema de criação de arquivos json

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const fs = require('fs')
 
 app.get('/', (req, res) => {
 
@@ -7,4 +8,42 @@ app.get('/', (req, res) => {
 
 })
 
+app.get('/showFS/:fs/', (req, res) => {
+
+    let fs = lerFS(req.params.fs)
+
+    console.log(fs.conteudo)
+
+    res.send(fs.conteudo)
+
+})
+
+app.get('/createFS/:fs/:conteudo', (req, res) => {
+
+    criarFS(req.params.fs, req.params.conteudo)
+
+    res.redirect('/')
+
+})
+
+app.get('/pages/:page', (req, res) => {
+
+    res.sendFile(`${__dirname}/assets/pages/${req.params.page}.html`)
+
+})
+
 app.listen(process.env.PORT || 8080, () => console.log('Rodando interface'))
+
+function criarFS(name, conteudo){
+
+    fs.createWriteStream(`${__dirname}/fs/${name}.json`).write(`{ "conteudo": "${conteudo}" } `)
+
+}
+
+function lerFS(name){
+
+    let arq = JSON.parse(fs.readFileSync(`${__dirname}/fs/${name}.json`))
+
+    return arq
+
+}
