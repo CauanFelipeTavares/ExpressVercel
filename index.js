2558e62a66a30542e7ca9f44264d413633d64dca
Kali <tavarescauanf@gmail.com>
2023-04-13 17:02:10 -0300

    Teste para websocket

--- index.js
+++ index.js
@@ -21,4 +21,26 @@ app.get('/banco/:sticker', function (req, res) {
   res.sendFile(path.join(__dirname + `/sticker/${req.params.sticker}.webp`))
 })
 
-app.listen(80, () => console.log('Rodando interface'))
+app.get('/ws', (req, res) => {
+
+  res.sendFile(__dirname + '/assets/pages/websocket.html')
+
+})
+
+const server = app.listen(80, () => console.log('Rodando interface'))
+
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ server: server, path: '/ws' }) //
+
+wss.on('connection', (ws) => {
+
+  console.log('Nova conexão WebSocket estabelecida')
+
+  ws.on('message', (event) => {
+
+    console.log('EVENT:', event)
+
+  })
+
+});
